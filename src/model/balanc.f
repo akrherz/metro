0aa1533b95fb6dacaf8ac0dd1be6f966788d0935
Miguel Tremblay <framagit.miguel@ptaff.ca>
2007-02-15 19:33:37 +0000

    Translation of comments.

--- src/model/balanc.f
+++ src/model/balanc.f
@@ -34,6 +34,9 @@
 *
 
 ***
+*     Subroutine BALANC: perform the road condition forecast for one station
+*                         using the energy balance and the condition of the
+*                         surface.
 *     Sous-routine BALANC: effectue la prevision des conditions routieres
 *                         (pour une station) a partir du bilan energetique 
 *                         de la surface et des conditions de surface.
@@ -62,37 +65,37 @@
 *     DEFINITIONS     *
 ***                 ***
 ***
-*     Entrees
+*     Input
 *     -------
-*     FS: Flux solaire incident (W)
-*     FI: Flux infra-rouge incident (W)
-*     P0: Pression de surface (Pa)
-*     TA: Temperature de l'air (C) au niveau ZT
-*     QA: Humidite specifique (g/kg) au niveau ZT
-*     VA: Vent (m/s) au niveau ZU
-*     TYP: Type probable de precipitation ( 1 -> pluie, 2 -> neige )
-*     FT: "Forecast time" (heures GMT)
-*     PR: Taux de precipitation (m/s)
-*     NTP: Indice de debut de la prevision
-*     NTFM: Indice de fin de la prevision
-*     CNT: Constantes de la grille
-*     Z0: Longueur de rugosite (m)
-*     Z0T: Longueur de rugosite (m)
-*     ZU: Hauteur du niveau de la prevision de vent (m)
-*     ZT: Hauteur du niveau de la prevision de temperature de d'humidite (m)
-*     LCORR: Decalage horaire pour retrouver l'heure local (h)
-*     EPSILON: Emissivite de la route
-*     TS0: Temperature cible pour la fin du couplage (C)
-*     FCOR: Facteur de Coriolis
-*     WW: Vents minimum le jour et la nuit (m/s)
-*     WA: Flux "anthropogenique"
-*     ALN: Albedo de la neige
-*     ALR: Albedo de la route 
-*     FP: Point de congelation (C)
-*     FLAT: Specifie si pont ou non
-*     ITP: Profil de temperature de la route (C, iref niveaux != 0)
-*     FSCORR: Coefficient de couplage du flux solaire
-*     FICORR: Coefficient de couplage du flux infra-rouge
+*     FS: Incident solar flux (W)
+*     FI: Incident infra-red flux (W)
+*     P0: Surface pressure (Pa)
+*     TA: Air temperature (C) at level ZT
+*     QA: Specific humidity (g/kg) at level ZT
+*     VA: Wind (m/s) at level ZU
+*     TYP: Probable type of precipitation ( 1 -> rain, 2 -> snow )
+*     FT: "Forecast time" (GMT)
+*     PR: Precipitation rate (m/s)
+*     NTP: Index for the start of coupling
+*     NTFM: Index for the end of forecast
+*     CNT: Grid constants
+*     Z0: Roughness length (m)
+*     Z0T: Roughness length (m)
+*     ZU: Height of level of the wind forecast (m)
+*     ZT:  Height of level of the atmospheric forecast (m)
+*     LCORR: time shift to retrieve local time
+*     EPSILON: Road emissivity
+*     TS0: Target temperature for the end of coupling (C)
+*     FCOR: Coriolis factor
+*     WW: Minimum winds for the day and the night (m/s)
+*     WA: Anthropogenic flux
+*     ALN: Snow Albedo 
+*     ALR: Road Albedo 
+*     FP: Frozing point (C)
+*     FLAT: Road or bridge
+*     ITP: Road temperature profile (C, iref levels != 0)
+*     FSCORR: Coupling coefficient of solar flux
+*     FICORR: Coupling coefficient of infra-red flux
 *     iref:  number of grid levels 
 *     ir40: level at 40 cm depth 
 ***
@@ -114,9 +117,9 @@
       DOUBLE PRECISION dpFP(DTMAX), dpSST(DTMAX)
       INTEGER npRC(DTMAX)
 ***
-*     Entrees/Sorties
+*     Input/Output
 *     ---------------
-*     FLUX Matrice contenant differents champs de sortie
+*     FLUX Matrix contening the output fields
 *         FLUX(1,i): FT(i)
 *         FLUX(2,i): TA(i)
 *         FLUX(3,i): VA(i)
@@ -125,16 +128,16 @@
 *         FLUX(6,i): ER1(i)
 *         FLUX(7,i): ER2(i)
 *         FLUX(8,i): PR(i)
-*         FLUX(9,i): encore indeterminee
-*         FLUX(10,i): etat de la route
-*                     Code: 1. -> Pave sec
-*                           2. -> Pave mouille
-*                           3. -> Glace
-*                           4. -> Rosee (Dew)
-*                           5. -> Gel (Frost)
-*                           6. -> Pluie verglacante
-*                           7. -> Neige
-*     TS: Serie temporelle de temperature de surface
+*         FLUX(9,i): not used
+*         FLUX(10,i): Road condition
+*                     Code: 1. -> Dry road
+*                           2. -> Wet road
+*                           3. -> Ice
+*                           4. -> Dew
+*                           5. -> Frost
+*                           6. -> Freezing rain
+*                           7. -> Snow
+*     TS:Time series of surface temperature
 ***
       LOGICAL ECHEC
       DOUBLE PRECISION ER1, ER2
@@ -150,13 +153,13 @@
       REAL CL, PR1, PR2, DX, PRG, FZ
       REAL COFS, COFI
 ***
-*     Variables de FLXSURFZ
+*     Variables of FLXSURFZ
 *     ---------------------
 ***
       REAL CMU, CTU, RIB, ILMO, FTEMP, FVAP
       REAL H, UE, LZZ0, LZZ0T, fm, fh
 ***
-*     Initialisation du common CLELOG
+*     Initialization of common CLELOG
 *     -------------------------------
 ***
 
@@ -243,10 +246,10 @@
          WRITE(*,*) "DEBUT BALANC"
       end if
 
-**     Conversion du array en matrice
+**    Conversion of array in matrix
       CALL ARRAY2MATRIXDOUBLEPRECISION(CNT_IN, CNT , n, 2)
 
-*     Initialisation de parametres
+*     Initialisation of parametres
 *     ----------------------------
       FZ = 0.1
       H = 300.0
@@ -254,28 +257,28 @@
       ILMO = 1.
       next = 1
       now = 2
-*     Initialisation de la variation d'albedo en fonction de la neige
+*     Initialization of the albeda variation depending of the snow
 *     ---------------------------------------------------------------
 *     |                                                             |
-*     |Fonction utilisee:                                           |
+*     |Functions used:                                           |
 *     |                                                             |
 *     | AL = max( ALR , min( ALN , M*ER2+ALR-M ) )                  |
 *     |                                                             |
 *     | ou                                                          | 
-*     |     M = ( ALN - ALR ) / 5 millimetres d'equivalent-eau      |
+*     |     M = ( ALN - ALR ) / 5 millimetres water equivalent      |
 *     |                                                             |
 *     | i.e.                                                        |
 *     |                                                             |
-*     |      l'albedo varie lineairement de ALR a ALN pour ER2      |
-*     |      compris entre 1 et 6 millimetres                       |
+*     |      albedo change linearly from ALR to ALN for ER2         |
+*     |      inside  1 and 6 millimetres                            |
 *     |                                                             |
-*     |      AL = ALR pour ER2 < 1 mm                               |
+*     |      AL = ALR for  ER2 < 1 mm                               |
 *     |                                                             |
-*     |      AL = ALN pour ER2 > 6 mm                               |
+*     |      AL = ALN for  ER2 > 6 mm                               |
 *     |                                                             |
 *     ---------------------------------------------------------------
       M = (ALN - ALR) / 5.
-*     Initialisation du profil de temperature
+*     Initialization of temperature profile
 *     ---------------------------------------
       G(0) = 0.0
       do j=1,iref
@@ -284,18 +287,18 @@
          G(j) = 0.
       end do
 *     ++++++++++++++++++++++++++++++++++
-*      Entree de la boucle de prevision
+*      Beginning of the forecast loop
 *     ++++++++++++++++++++++++++++++++++
       do i = NTP+1,NTFM
-*        Preparation des differents termes du bilan
+*        Initialization of the differents balance terms
 *        ++++++++++++++++++++++++++++++++++++++++++
 *        road temp in Kelvins
          TSK = T(1,now) + TCDK
-*        radiation corps noir
+*        black body radiation
          RA = EPSILON*STEFAN*TSK**4
-*        humidite a la suface
+*        humidite at the suface
          call SRFHUM  ( QG, CL, ER1, ER2, TSK, P0(i), QA(i), FP )
-*        densite de l'air au sol
+*        Air density at the surface
          ier = CHKDIV ( RGASD * FOTVT ( TSK , QG ), "balanc.ftn", 196 )
          if ( ier .eq. 1 ) then
             WRITE(*,*) "Echec dans balanc.f"
@@ -304,30 +307,30 @@
          end if
          RHO = P0(i) / ( RGASD * FOTVT ( TSK , QG ) )
 
-*        modification du vent
+*        wind modification 
          call VENMIN  ( WW, FT(i), VA(i) )
-*        energie utilisee/liberee par neige fondante/pluie verglacante
+*        energy used/freed by the melting snow/freezing rain
          call VERGLAS ( TYP(i), T(1,now), FP, FZ, PR(i), PR1, PR2, PRG )
-*        coefficients des flux de chaleur
+*        coefficients of heat flux
          call FLXSURFZ( CMU  , CTU , RIB  , FTEMP, FVAP ,
      *                  ILMO, UE   , FCOR , TA(i)+TCDK,
      *                  QA(i), ZU, ZT  , VA(i), TSK  , QG   ,
      *                  H    , Z0  , Z0T  , LZZ0 , LZZ0T,
      *                  FM   , FH  ,  1,  1     )
-*        albedo efficace
+*        effective albedo 
          AL = 1.0 - max( ALR, min( ALN,M*ER2+ALR-M ) )
-*        relaxation des coefficients de couplage des flux
+*        relaxation of coefficients of flux coupling
          COFS = 1.0 + FSCORR*exp((-DT)*real(i-NTP)/(4.*3600.))
          COFI = 1.0 + FICORR*exp((-DT)*real(i-NTP)/(4.*3600.))
-*        Calcul du bilan energetique
+*        Computing of energy balance
 *        +++++++++++++++++++++++++++
-*         (COFS*AL*FS(i)) : Flux solaire absorbé
-*         RA : Flux montant infra-rouge
-*         COFI*EPSILON*FI(i) : Flux infrarouge absorbé
+*         (COFS*AL*FS(i)) : Absorbed solar flux
+*         RA : Outgoing infra-red flux
+*         COFI*EPSILON*FI(i) : Absorbed infra-red flux
 *         RHO*CTU*( CPD*(TA(i)-T(1,now))) FC
 *         RHO*CTU*(CL*(QA(i)-QG)) : FV
-*         PRG : Flux du changement de phase
-*         WA : Flux anthropogénique
+*         PRG : Phase change flux
+*         WA : Anthropogenic flux
          G(0) = max(0.0,REAL(COFS*AL*FS(i))) - RA + COFI*EPSILON*FI(i)
      *        + RHO*CTU*( CPD*(TA(i)-T(1,now)) + CL*(QA(i)-QG)) + PRG
      *        + WA
@@ -343,7 +346,7 @@
          dpSST(i) = T(ir40, now)
          G(1) = CNT(1,1) * ( T(2,now) - T(1,now) )
 
-*        Transition de phase lors d'un passage par le point de fusion
+*        Phase Transition when passing the melting point
 *        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
          if ( T(1,next).lt.FP .and. T(1,now).ge.FP ) then
 *           Degel, G(0) positif 
@@ -352,8 +355,7 @@
 *           Gel, G(0) negatif
             FGD = ER1 * CHLF
          end if
-*        NOTE: G(0) = G(0) + G(1) lorsque FGD .ne. 0.0 pour
-*        conservation d'energie
+*        NOTE: G(0) = G(0) + G(1) when FGD .ne. 0.0 for energy conservation
          if ( FGD.lt.0.0 .and. G(0).gt.0.0) then
             G(0) = G(0) - G(1)
             FGD = min( 0.0, FGD + DT*G(0) )
@@ -368,10 +370,10 @@
             DX = 0.0
             T(1,next) = T(1,now)+DT*(CNT(1,2)*( G(1) - G(0)))
          end if
-*        Calculer l'evolution des temperatures dans le sol
+*        Calculation of temperature evolution in the ground
 *        +++++++++++++++++++++++++++++++++++++++++++++++++
          call TSEVOL ( T, iref, now, CNT, G, FLAT, TA(i) )
-*        Bilan d'accumulations au sol
+*        Accumulation balance at the surface
 *        ++++++++++++++++++++++++++++
 
          call RODCON ( ER1, ER2, RHO  , CTU, CL , FP , FZ ,
@@ -381,13 +383,13 @@
          dpRT(i) = T(1,next)
          dpRA(i) = ER1
          dpSN(i) = ER2
-*        Inversion des indices
+*        Inversion of indices
 *        +++++++++++++++++++++
          next = 3 - next
          now = 3 - now
       end do
       
-**     Conversion de la matrice en array
+**     Conversion of matrix in array
       CALL MATRIX2ARRAYDOUBLEPRECISION(CNT, CNT_IN, n, 2)
 
       if( .not. bSilent) then
