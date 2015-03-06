9277e77e5cdd044f4e50d322257bfab1e477564c
Francois Fortin <francois.fortin@gmail.com>
2015-03-06 04:30:10 +0000

    bump version number to 3.2.9

--- src/frontend/metro_config.py
+++ src/frontend/metro_config.py
@@ -68,8 +68,8 @@ METRO_CONFIG_GETTEXT_PACKAGE  = "metro_config"
 METRO_CONFIG_GETTEXT_LOCALEDIR = "./locale"
 
 # Constant definition 
-CFG_METRO_VERSION="3.2.8"
-CFG_METRO_DATE="2015-02-14"
+CFG_METRO_VERSION="3.2.9"
+CFG_METRO_DATE="2015-03-05"
 
 
 # Origin of the value (command line, config file, hardcoded value,
