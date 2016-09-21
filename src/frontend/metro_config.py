2dc963486be84aaaeb8cf36b2e1423262a7775ac
Francois Fortin <francois.fortin@gmail.com>
2016-09-21 03:27:48 +0000

    preparing release 3.3.1

--- src/frontend/metro_config.py
+++ src/frontend/metro_config.py
@@ -68,8 +68,8 @@ METRO_CONFIG_GETTEXT_PACKAGE  = "metro_config"
 METRO_CONFIG_GETTEXT_LOCALEDIR = "./locale"
 
 # Constant definition 
-CFG_METRO_VERSION="3.3.0"
-CFG_METRO_DATE="2015-03-10"
+CFG_METRO_VERSION="3.3.1"
+CFG_METRO_DATE="2016-09-20"
 
 
 # Origin of the value (command line, config file, hardcoded value,
